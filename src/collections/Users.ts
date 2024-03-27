5b28441d7174ad0eb3d5a380b7c93f2ac5285528
quelq-un <quelquun117@gmail.com>
2024-03-27 17:02:42 -0300

    first commit

---
+++ src/collections/Users.ts
@@ -0,0 +1,21 @@
+//import { PrimaryActionEmailHtml } from '../components/emails/PrimaryActionEmail'
+import { Access, CollectionConfig } from 'payload/types'
+
+export const Users: CollectionConfig = {
+    slug: 'users',
+    auth: true,
+    access: {
+        read: () => true,
+        create: () => true,
+    },
+    fields: [
+     {
+        name: 'role',
+        type: 'select',
+        options: [
+            { label: 'Admin', value: 'admin' },
+            { label: 'User', value: 'user' },
+        ],
+    },
+ ],
+}
---
+++ src/trpc/trpc.ts
@@ -0,0 +1,10 @@
+// import { User } from '@/payload-types'
+// import { ExpressContext } from '@/server'
+
+import { TRPCError, initTRPC } from '@trpc/server'
+import { PayloadRequest } from 'payload/types'
+
+const t = initTRPC.context().create()
+
+export const router = t.router
+export const publicProcedure = t.procedure
