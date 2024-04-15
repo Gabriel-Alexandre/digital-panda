a58dd04200401af1b616c97b4577f3b869f5b36f
quelq-un <quelquun117@gmail.com>
2024-04-14 22:34:52 -0300

    sem domains

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-
-		domains: [
-			'localhost',
-			'digitalhippo-production-e05a.up.railway.app',
-			
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+			},
+			{
+				protocol: "https",
+				hostname: "digitalhippo-production.up.railway.app",
+			},
 		],
 	},
 };
