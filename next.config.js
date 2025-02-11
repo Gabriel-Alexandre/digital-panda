f8e6bc1b10033c29cbaa5ba1bb9a267107233d91
Gabriel-Alexandre <biellalex86@gmail.com>
2025-02-11 20:06:40 -0300

    new test

--- next.config.js
+++ next.config.js
@@ -3,23 +3,10 @@ const nextConfig = {
 	images: {
 		domains: [	
 				"localhost",
-			     "digitalpanda.vercel.app",
-			     "vercel.app"
+			    "https://digital-panda.vercel.app/"
 			
 		],
 	},
-	async rewrites() {
-		return [
-			{
-				source: '/api/:path*',
-				destination: '/api/:path*',
-			},
-			{
-				source: '/sell/:path*',
-				destination: '/api/sell/:path*',
-			}
-		]
-	}
 };
 
 module.exports = nextConfig;
