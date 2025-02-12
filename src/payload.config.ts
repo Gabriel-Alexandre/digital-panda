a818d1d7f0f39ceb324ca7e8607d2cdb6dcfe7f6
Gabriel-Alexandre <biellalex86@gmail.com>
2025-02-12 20:27:14 -0300

    new changes

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -4,19 +4,21 @@ import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { slateEditor } from '@payloadcms/richtext-slate'
 import path from 'path'
 import { Users } from './collections/Users'
- import dotenv from 'dotenv'
- import { Products } from './collections/Products/Products'
+import dotenv from 'dotenv'
+import { Products } from './collections/Products/Products'
 import { Media } from './collections/Media'
 import { ProductFiles } from './collections/ProductFile'
- import { Orders } from './collections/Orders'
+import { Orders } from './collections/Orders'
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
 })
 
 export default buildConfig({
-  serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-  collections: [Users,Products, Media, ProductFiles , Orders], 
+  serverURL: process.env.NEXT_PUBLIC_SERVER_URL 
+    ? `${process.env.NEXT_PUBLIC_SERVER_URL}`
+    : 'http://localhost:3000',
+  collections: [Users, Products, Media, ProductFiles, Orders],
   routes: {
     admin: '/sell',
   },
